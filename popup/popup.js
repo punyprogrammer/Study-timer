0b26ce3c247cb8ac80afa49a2d88d77d50557fd8
Amardeep Ganguly <you@example.com>
2021-10-18 07:38:03 +0530

    Added CRUD functionality

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,44 @@
+const tasks=[]
 const addTaskBtn =document.getElementById("add-task-btn")
-addTaskBtn.addEventListener("click",()=>{
-const taskRow=document.createElement("div");
-const text =document.createElement("input")
-text.type="text"
-text.placeholder="Enter a new task"
-const deleteBtn =document.createElement("input")
-deleteBtn.type="button"
-deleteBtn.value="X"
-taskRow.appendChild(text)
-taskRow.appendChild(deleteBtn)
-const taskContainer=document.getElementById("task-container")
-taskContainer.append(taskRow)
-
-})
+addTaskBtn.addEventListener("click",()=>addTask())
+function renderTask(taskNum){
+    const taskRow=document.createElement("div");
+    const text =document.createElement("input")
+    text.type="text"
+    text.placeholder="Enter a new task"
+    text.value=tasks[taskNum]
+    text.addEventListener("change",()=>{
+    tasks[taskNum]=text.value;
+    console.log(tasks)
+    })
+    const deleteBtn =document.createElement("input")
+    deleteBtn.type="button"
+    deleteBtn.value="X"
+    deleteBtn.id='add-task-btn'
+    deleteBtn.addEventListener('click',()=>{
+    deleteTask(taskNum)
+    })
+    taskRow.appendChild(text)
+    taskRow.appendChild(deleteBtn)
+    const taskContainer=document.getElementById("task-container")
+    taskContainer.append(taskRow)
+}
+function addTask()
+{
+        const taskNum=tasks.length
+        console.log(taskNum)
+        renderTask(taskNum)
+        
+}
+function deleteTask(taskNum)
+{
+    tasks.splice(taskNum,1);
+    renderTasks()
+}
+function renderTasks(){
+    const taskContainer=document.getElementById('task-container')
+    taskContainer.textContent=""
+    tasks.forEach((taskText,taskNum)=>{
+    renderTask(taskNum)
+    })
+}
